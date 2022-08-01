bffd8e3618213fc54210fc3c747fa3075e31872b
Eric Toner <erict@aweber.com>
2022-07-31 21:37:02 -0400

    * Create and combine initial views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eric Toner on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
